e82fb39158c2e68127d062ccb921d350007a809c
Maleek Mustapha <mustapharhokeeb419@gmail.com>
2023-10-13 00:14:59 +0100

    Repo_Update

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,jsonify
+
+
+app = Flask(__name__)
+
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data analyst",
+        "location": "San Fransisco",
+        "salary":"100$"
+    },
+     {
+        "id": 2,
+        "title": "Cloud Engineer",
+        "location": "Remote",
+    },
+
+     {
+        "id": 3,
+        "title": "Full-Statck_Developer",
+        "location": "Lagos,Nigeria",
+        "salary": "200$"
+    }
+]
+
+@app.route("/")
+
+def hello_Harby():
+    return render_template("index.html",jobs = JOBS,companyname = "harby")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+   return jsonify(JOBS)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
